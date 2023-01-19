331e04932e1a031f906af5abe3195469ca433bf0
liu00569 <liu0569@algonquin.com>
2023-01-19 13:27:29 -0500

    updates

--- main.js
+++ main.js
@@ -1,7 +1,16 @@
-console.log("Hello")
+console.log("temLiterals")
+
+
 
 const firstName = "Jane"
 
 const lastName = "Doe"
 
-const temLiteral = `My first name is: $(firstName), My last name: $(lastName), $(2+2)`
+const temLiterals = `My first name is: $(firstName), My last name: $(lastName), $(2+2)`
+
+
+let points = 5
+
+let bullets = -3.5
+
+console.log("points + bullets")
